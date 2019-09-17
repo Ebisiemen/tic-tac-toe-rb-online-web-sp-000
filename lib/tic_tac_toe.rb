fd4e619440c46afbfb6060755f820c50dc75228a
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-17 13:34:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,6 +77,9 @@ def turn(board)
   end   
 end   
 
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
 # def position_taken?(board, index)
 #   board[index] == 'X' || board[index] == 'O'
 # end
