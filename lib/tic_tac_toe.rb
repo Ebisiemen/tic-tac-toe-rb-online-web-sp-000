37119b1bc3dbfc3cd9fc5e3e71abd909fa75b7d0
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-17 13:32:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,6 +85,15 @@ def current_player(board)
   turn_count(board).even? ? 'X' : 'O'
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |element|  
+  if element == "X" || element == "O" 
+    counter += 1
+  end 
+  end
+  counter 
+end 
 # def turn_count(board)
 #   board.count { |token| token == 'X' || token == 'O' }
 # end
