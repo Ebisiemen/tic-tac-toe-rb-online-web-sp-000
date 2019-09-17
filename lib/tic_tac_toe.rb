2e3e1ddab636637d9f230fb7be0a514691a1f7be
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-17 13:34:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,9 +77,9 @@ def turn(board)
   end   
 end   
 
-def position_taken?(board, index)
-  board[index] == 'X' || board[index] == 'O'
-end
+# def position_taken?(board, index)
+#   board[index] == 'X' || board[index] == 'O'
+# end
 
 def current_player(board)
   turn_count(board).even? ? 'X' : 'O'
